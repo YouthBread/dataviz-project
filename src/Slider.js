07900e263365278cd891730cd49651e2031388a5
Youth Bread <YouthBread@users.noreply.github.com>
2017-10-24 20:43:54 -0400

    add stats table but not support general update pattern

--- src/Slider.js
+++ src/Slider.js
@@ -4,12 +4,8 @@ function Slider () {
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
 
-    var slider_axis = court.append('g');
-    var slider_rect = court.append('g');
 
-    slider_axis.attr("width", innerWidth)
-          .attr("height", innerHeight)
-          .attr('transform', `translate(0, ${innerHeight+20})`)
+    slider_axis.attr('transform', `translate(0, ${innerHeight+20})`)
 
     slider_rect.attr("width", innerWidth)
           .attr("height", innerHeight)
@@ -17,13 +13,15 @@ function Slider () {
 
     var minDate = new Date('1997'),
         scale = d3.scaleTime()
-            .domain([minDate, d3.timeYear.offset(minDate, 18)])
+            .domain([minDate, d3.timeYear.offset(minDate, 20)])
             .range([margin.left, innerWidth])
             .clamp(true),
         format = d3.timeFormat('%Y');
 
     updateHeader(minDate)
     add_shot(minDate)
+    Stat_Table(minDate);
+
 
 
     slider_axis
@@ -35,27 +33,28 @@ function Slider () {
         .call(d3.drag().on('drag', dragged));
 
     var rectWidth = 8;
-    var rect = slider_rect.append("rect")
-                     .attr("x", margin.left)
-                     .attr("y", 0)
-                     .attr("width", rectWidth)
-                     .attr("height", 20);
+    rect_entity.attr("x", margin.left)
+               .attr("y", 0)
+               .attr("width", rectWidth)
+               .attr("height", 20);
 
 
     function updateHeader(date) {
-        d3.select('#header').text(format(date)+'-'+(date.getFullYear()+1).toString()+' season');
+        title.text(format(date)+'-'+(date.getFullYear()+1).toString()+' season shoting map')
+                            .attr('x', margin.left)
+                            .attr('y', margin.top);
     }
 
 
     function dragged(d) {
         const parseTime = d3.timeParse("%Y");
-        var prev = d3.select('#header').text().split('-')[0]
-        var x = Math.min(Math.max(d3.event.x, margin.left), innerWidth);
+        var prev = title.text().split('-')[0]
+        var x = Math.min(d3.event.x, innerWidth-margin.left);
                 value = scale.invert(x);
 
         d3.select('.slider').attr('transform', 'translate(' + x + ',' + innerHeight + ')');
         updateHeader(value);
-        if (value.getFullYear() != parseTime(prev).getFullYear()) add_shot(value);
+        if (value.getFullYear() != parseTime(prev).getFullYear()) add_shot(value); Stat_Table(value);
     }
 }
 
