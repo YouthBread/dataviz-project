0e5d2d0db82a615b173b3d039def8807850ef82c
Youth Bread <YouthBread@users.noreply.github.com>
2017-11-04 21:27:29 -0400

    add injury line indication

--- src/Line.js
+++ src/Line.js
@@ -1,6 +1,6 @@
 //Fixed plot
 function Shot_Stat_Line(col, position){
-      const margin = { left: 60, right: 60, top: 20, bottom: 20 };
+      const margin = { left: 80, right: 60, top: 20, bottom: 20 };
 
       const width = 750;
       const height = 400;
@@ -25,37 +25,37 @@ function Shot_Stat_Line(col, position){
         .tickSize(5);
 
       var temp_g = d3.select("#"+position);
-      temp_g.attr('width', width)
-            .attr('height', height);
+      temp_g.attr("width", width)
+            .attr("height", height);
       const xAxisG = d3.select("#"+position).select("#x_axis")
-          .attr('transform', `translate(${0}, ${innerHeight})`);
+          .attr("transform", `translate(${0}, ${innerHeight})`);
       const yAxisG = d3.select("#"+position).select("#y_axis")
-          .attr('transform', `translate(${margin.left}, ${0})`);
+          .attr("transform", `translate(${margin.left}, ${0})`);
 
-      xAxisG.select('#x_label')
-          .attr('x', innerWidth / 2)
-          .attr('y', 40)
-          .text('Year');
+      xAxisG.select("#x_label")
+          .attr("x", innerWidth / 2)
+          .attr("y", 40)
+          .text("Year");
 
-      yAxisG.select('#y_label')
-          .attr('x', -innerHeight / 2)
-          .attr('y', -35)
-          .attr('transform', `rotate(-90)`)
-          .style('text-anchor', 'middle')
+      yAxisG.select("#y_label")
+          .attr("x", -innerHeight / 2)
+          .attr("y", -60)
+          .attr("transform", `rotate(-90)`)
+          .style("text-anchor", "middle")
           .text(col);
 
 
 
       var line = d3.line()
               .curve(d3.curveMonotoneX)
-              .x(function(d) {return xScale(parseTime(d.Season.split('-')[0]))})
+              .x(function(d) {return xScale(parseTime(d.Season.split("-")[0]))})
               .y(function(d) {return yScale(d[col])});
 
 
-      d3.csv('data/stat.csv', data => {
+      d3.csv("data/stat.csv", data => {
 
         xScale
-          .domain(d3.extent(data, d=>parseTime(d.Season.split('-')[0])))
+          .domain(d3.extent(data, d=>parseTime(d.Season.split("-")[0])))
           .range([margin.left, innerWidth])
           .nice();
 
@@ -65,9 +65,9 @@ function Shot_Stat_Line(col, position){
           .nice();
 
 
-        temp_g.append('path')
+        temp_g.append("path")
            .attr("fill", "none")
-           .attr('stroke', 'steelblue')
+           .attr("stroke", "steelblue")
            .attr("stroke-linejoin", "round")
            .attr("stroke-linecap", "round")
            .attr("stroke-width", 2)
@@ -75,13 +75,13 @@ function Shot_Stat_Line(col, position){
 
 
 
-        temp_g.selectAll('circle').data(data)
-          .enter().append('circle')
-            .attr('cx', d => xScale(parseTime(d.Season.split('-')[0])))
-            .attr('cy', d => yScale(d[col]))
-            .attr('fill', 'blue')
-            .attr('fill-opacity', 0.6)
-            .attr('r', 3)
+        temp_g.selectAll("circle").data(data)
+          .enter().append("circle")
+            .attr("cx", d => xScale(parseTime(d.Season.split("-")[0])))
+            .attr("cy", d => yScale(d[col]))
+            .attr("fill", "blue")
+            .attr("fill-opacity", 0.6)
+            .attr("r", 3)
             .on("mouseover", function(d) {
                 tool_tips.transition()
                     .duration(200)
@@ -104,10 +104,10 @@ function Shot_Stat_Line(col, position){
 
 //Plot with general update
 function Shot_Accu_Line(year,position){
-      var format = d3.timeFormat('%Y');
+      var format = d3.timeFormat("%Y");
 
-      var target_season = format(year)+'-'+(year.getFullYear()+1).toString().substring(2, 4);
-      const margin = { left: 60, right: 60, top: 20, bottom: 20 };
+      var target_season = format(year)+"-"+(year.getFullYear()+1).toString().substring(2, 4);
+      const margin = { left: 80, right: 60, top: 20, bottom: 20 };
 
       const width = 750;
       const height = 400;
@@ -132,28 +132,28 @@ function Shot_Accu_Line(year,position){
         .tickSize(5);
 
       var temp_g = d3.select("#"+position);
-      temp_g.attr('width', width)
-            .attr('height', height);
+      temp_g.attr("width", width)
+            .attr("height", height);
       const xAxisG = d3.select("#"+position).select("#x_axis")
-          .attr('transform', `translate(${0}, ${innerHeight})`);
+          .attr("transform", `translate(${0}, ${innerHeight})`);
       const yAxisG = d3.select("#"+position).select("#y_axis")
-          .attr('transform', `translate(${margin.left}, ${0})`);
+          .attr("transform", `translate(${margin.left}, ${0})`);
 
-      xAxisG.select('#x_label')
-          .attr('x', innerWidth / 2)
-          .attr('y', 40)
-          .text('Year');
+      xAxisG.select("#x_label")
+          .attr("x", innerWidth / 2)
+          .attr("y", 40)
+          .text("Year");
 
-      yAxisG.select('#y_label')
-          .attr('x', -innerHeight / 2)
-          .attr('y', -35)
-          .attr('transform', `rotate(-90)`)
-          .style('text-anchor', 'middle')
-          .text('Accuracy');
+      yAxisG.select("#y_label")
+          .attr("x", -innerHeight / 2)
+          .attr("y", -60)
+          .attr("transform", `rotate(-90)`)
+          .style("text-anchor", "middle")
+          .text("Accuracy");
 
 
 
-      d3.csv('data/kobe.csv', data => {
+      d3.csv("data/kobe.csv", data => {
           var temp_data = data.filter(d=>d.season == target_season)
 
            temp_data = d3.nest()
@@ -166,7 +166,7 @@ function Shot_Accu_Line(year,position){
 
           for (i = 0; i < temp_data.length; i++) {
             if (temp_data[i].values[0]) {
-              if (temp_data[i].values[0].key == '1') {
+              if (temp_data[i].values[0].key == "1") {
                 made = temp_data[i].values[0].value
               } else {
                 miss = temp_data[i].values[0].value
@@ -174,7 +174,7 @@ function Shot_Accu_Line(year,position){
             }
 
             if (temp_data[i].values[1]) {
-              if (temp_data[i].values[1].key == '1') {
+              if (temp_data[i].values[1].key == "1") {
                 made = temp_data[i].values[1].value
               } else {
                 miss = temp_data[i].values[1].value
@@ -193,38 +193,39 @@ function Shot_Accu_Line(year,position){
             .range([margin.left, innerWidth])
             .nice();
 
+          var height = d3.extent(temp_data, d=>+d.values);
           yScale
             .domain(d3.extent(temp_data, d=>+d.values))
             .range([innerHeight, margin.top])
             .nice();
 
-          var path_made = temp_g.selectAll('#line_path').data([temp_data])
+          var path_made = temp_g.selectAll("#line_path").data([temp_data])
           path_made.exit().transition(t).remove();
 
           path_made
-             .enter().append('path')
+             .enter().append("path")
              .merge(path_made)
              .transition(t)
-             .attr('id', 'line_path')
+             .attr("id", "line_path")
              .attr("fill", "none")
-             .attr('stroke', 'steelblue')
+             .attr("stroke", "steelblue")
              .attr("stroke-linejoin", "round")
              .attr("stroke-linecap", "round")
              .attr("stroke-width", 2)
              .attr("d", line(temp_data));
 
 
-          var circles_made = temp_g.selectAll('#line_circle').data(temp_data)
+          var circles_made = temp_g.selectAll("#line_circle").data(temp_data)
           circles_made.exit().transition(t).remove();
 
 
           circles_made
-            .enter().append('circle')
+            .enter().append("circle")
             .merge(circles_made)
-             .attr('id', 'line_circle')
-             .attr('fill', 'blue')
-             .attr('fill-opacity', 0.6)
-             .attr('r', 3)
+             .attr("id", "line_circle")
+             .attr("fill", "blue")
+             .attr("fill-opacity", 0.6)
+             .attr("r", 3)
              .on("mouseover", function(d) {
                 tool_tips.transition()
                     .duration(200)
@@ -239,24 +240,59 @@ function Shot_Accu_Line(year,position){
                     .style("opacity", 0);
                   })
              .transition(t)
-             .attr('cx', d => xScale(parseTime(d.key)))
-             .attr('cy', d => yScale(d.values))
+             .attr("cx", d => xScale(parseTime(d.key)))
+             .attr("cy", d => yScale(d.values))
 
 
           xAxisG.call(xAxis);
           yAxisG.call(yAxis);
 
+
+          d3.csv("data/complete_injury.csv", data => {
+
+              var temp_data = data.filter(d=>d.season == target_season)
+              var injury = temp_g.selectAll("#injury_line").data(temp_data)
+              injury.exit().transition(t).remove();
+
+              injury.enter().merge(injury).append("line")
+                   .attr("id", "injury_line")
+                   .style("stroke", "red")
+                   .style("stroke-linecap", "round")
+                   .style("stroke-width", 1.5)
+                   .attr("x1", d => xScale(parseTime(d.game_date)))
+                   .attr("y1", yScale(height[0]))
+                   .attr("x2", d => xScale(parseTime(d.game_date)))
+                   .attr("y2", yScale(height[1]))
+                   .on("mouseover", function(d) {
+                          tool_tips.transition()
+                              .duration(200)
+                              .style("opacity", 1);
+                          tool_tips.html(d.Notes)
+                             .style("left", (d3.event.pageX - 20) + "px")
+                             .style("top", (d3.event.pageY - 20) + "px");
+                          })
+                  .on("mouseout", function(d) {
+                          tool_tips.transition()
+                              .duration(500)
+                              .style("opacity", 0);
+                    });
+
+              })
+
       });
+
+
+
 }
 
 
 //Plot with general update
 function Shot_Score_Line(year,position){
-      var format = d3.timeFormat('%Y');
+      var format = d3.timeFormat("%Y");
 
-      var target_season = format(year)+'-'+(year.getFullYear()+1).toString().substring(2, 4);
+      var target_season = format(year)+"-"+(year.getFullYear()+1).toString().substring(2, 4);
 
-      const margin = { left: 60, right: 60, top: 20, bottom: 20 };
+      const margin = { left: 80, right: 60, top: 20, bottom: 20 };
 
       const width = 750;
       const height = 400;
@@ -281,30 +317,30 @@ function Shot_Score_Line(year,position){
         .tickSize(5);
 
       var temp_g = d3.select("#"+position);
-      temp_g.attr('width', width)
-            .attr('height', height);
+      temp_g.attr("width", width)
+            .attr("height", height);
       const xAxisG = d3.select("#"+position).select("#x_axis")
-          .attr('transform', `translate(${0}, ${innerHeight})`);
+          .attr("transform", `translate(${0}, ${innerHeight})`);
       const yAxisG = d3.select("#"+position).select("#y_axis")
-          .attr('transform', `translate(${margin.left}, ${0})`);
+          .attr("transform", `translate(${margin.left}, ${0})`);
 
-      xAxisG.select('#x_label')
-          .attr('x', innerWidth / 2)
-          .attr('y', 40)
-          .text('Year');
+      xAxisG.select("#x_label")
+          .attr("x", innerWidth / 2)
+          .attr("y", 40)
+          .text("Year");
 
-      yAxisG.select('#y_label')
-          .attr('x', -innerHeight / 2)
-          .attr('y', -35)
-          .attr('transform', `rotate(-90)`)
-          .style('text-anchor', 'middle')
-          .text('Score');
+      yAxisG.select("#y_label")
+          .attr("x", -innerHeight / 2)
+          .attr("y", -60)
+          .attr("transform", `rotate(-90)`)
+          .style("text-anchor", "middle")
+          .text("Score");
 
       var t = d3.transition()
             .duration(750);
 
 
-      d3.csv('data/kobe.csv', data => {
+      d3.csv("data/kobe.csv", data => {
           var temp_data = data.filter(d=>d.season == target_season)
 
            temp_data = d3.nest()
@@ -324,37 +360,39 @@ function Shot_Score_Line(year,position){
             .range([margin.left, innerWidth])
             .nice();
 
+          var height = d3.extent(temp_data, d=>+d.value);
+
           yScale
             .domain(d3.extent(temp_data, d=>+d.value))
             .range([innerHeight, margin.top])
             .nice();
 
-          var path_made = temp_g.selectAll('#line_path').data([temp_data])
+          var path_made = temp_g.selectAll("#line_path").data([temp_data])
           path_made.exit().transition(t).remove();
 
           path_made
-             .enter().append('path')
+             .enter().append("path")
              .merge(path_made)
              .transition(t)
-             .attr('id', 'line_path')
+             .attr("id", "line_path")
              .attr("fill", "none")
-             .attr('stroke', 'steelblue')
+             .attr("stroke", "steelblue")
              .attr("stroke-linejoin", "round")
              .attr("stroke-linecap", "round")
              .attr("stroke-width", 2)
              .attr("d", line(temp_data));
 
-          var circles_made = temp_g.selectAll('#line_circle').data(temp_data)
+          var circles_made = temp_g.selectAll("#line_circle").data(temp_data)
           circles_made.exit().transition(t).remove();
 
 
           circles_made
-            .enter().append('circle')
+            .enter().append("circle")
             .merge(circles_made)
-              .attr('id', 'line_circle')
-              .attr('fill', 'blue')
-              .attr('fill-opacity', 0.6)
-              .attr('r', 3)
+              .attr("id", "line_circle")
+              .attr("fill", "blue")
+              .attr("fill-opacity", 0.6)
+              .attr("r", 3)
               .on("mouseover", function(d) {
                  tool_tips.transition()
                      .duration(200)
@@ -369,14 +407,52 @@ function Shot_Score_Line(year,position){
                      .style("opacity", 0);
                    })
               .transition(t)
-              .attr('cx', d => xScale(parseTime(d.key)))
-              .attr('cy', d => yScale(d.value));
+              .attr("cx", d => xScale(parseTime(d.key)))
+              .attr("cy", d => yScale(d.value));
 
+
+          d3.csv("data/complete_injury.csv", data => {
+
+              var temp_data = data.filter(d=>d.season == target_season)
+              var injury = temp_g.selectAll("#injury_line").data(temp_data)
+              injury.exit().transition(t).remove();
+
+              injury.enter().merge(injury).append("line")
+                   .attr("id", "injury_line")
+                   .style("stroke", "red")
+                   .style("stroke-linecap", "round")
+                   .style("stroke-width", 1.5)
+                   .attr("x1", d => xScale(parseTime(d.game_date)))
+                   .attr("y1", yScale(height[0]))
+                   .attr("x2", d => xScale(parseTime(d.game_date)))
+                   .attr("y2", yScale(height[1]))
+                   .on("mouseover", function(d) {
+                          tool_tips.transition()
+                              .duration(200)
+                              .style("opacity", 1);
+                          tool_tips.html(d.Notes)
+                             .style("left", (d3.event.pageX - 20) + "px")
+                             .style("top", (d3.event.pageY - 20) + "px");
+                          })
+                  .on("mouseout", function(d) {
+                          tool_tips.transition()
+                              .duration(500)
+                              .style("opacity", 0);
+                    });
+
+              })
 
 
           xAxisG.call(xAxis);
           yAxisG.call(yAxis);
 
+
+
       });
 
-}
+
+};
+
+
+
+
